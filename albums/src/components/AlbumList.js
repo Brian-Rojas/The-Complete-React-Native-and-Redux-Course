203a38d535124c93cd4f3dfa4bcf024c84e3595e
MatheusR42 <matheus.araujo.r42@gmail.com>
2018-01-02 10:54:55 -0200

    Handling Component Layout

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
@@ -18,11 +18,10 @@ class AlbumList extends Component {
     }
 
     render() {
-        console.log(this.state.albums)
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }
