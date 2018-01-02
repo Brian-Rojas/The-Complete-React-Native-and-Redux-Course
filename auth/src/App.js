cb2e73e8ea5aecbe8393628f3ebf35b8493d0be6
MatheusR42 <matheus.araujo.r42@gmail.com>
2018-01-02 13:09:51 -0200

    Firebase Client Setup

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/commom';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyAbCDAxA5nMEfdau02DQj8_5sqYdMSjlTU",
+            authDomain: "auth-e840b.firebaseapp.com",
+            databaseURL: "https://auth-e840b.firebaseio.com",
+            projectId: "auth-e840b",
+            storageBucket: "auth-e840b.appspot.com",
+            messagingSenderId: "283972026700"
+        });
+    }
     render() {
         return (
             <View>
