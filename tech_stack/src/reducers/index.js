8b04dfa0c60dc86b8f8d5c218ae3aae37780273c
MatheusR42 <matheus.araujo.r42@gmail.com>
2018-01-09 09:03:59 -0200

    Calling Action Creators

--- tech_stack/src/reducers/index.js
+++ tech_stack/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibrayReducer';
+import SelectionReducer from './SelectionReducer';
 
 export default combineReducers({
-    libraries: LibraryReducer
+    libraries: LibraryReducer,
+    selectedLibraryId: SelectionReducer
 });
