626d799b304cb9952551635b9c83a11be483e967
MatheusR42 <matheus.araujo.r42@gmail.com>
2018-01-11 09:18:36 -0200

    Finish Rendering Lists the Right Way

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text,
+        TouchableWithoutFeedback,
+        View,
+        LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
 
+    renderDescription() {
+        const { library, expanded } = this.props;
+        
+        if (expanded) {
+            return (
+                <Text>{library.description}</Text>
+            );
+        }
+    }
     render() {
         const { titleStyle } = styles;
-        const { title } = this.props.library;
-        console.log(this.props);
+        const { id, title } = this.props.library;
 
         return (
-            <CardSection>
-                <Text style={titleStyle}>
-                    {title}
-                </Text>
-            </CardSection>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -32,4 +54,12 @@ const styles = {
     }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+
+    return {
+        expanded
+    };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
